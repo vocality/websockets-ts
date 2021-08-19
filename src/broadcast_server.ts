fc2be3382216c27f7b1f949dc362546a843bd259
vocality <infos@vocality.fr>
2021-08-19 15:11:18 +0200

    Update with Express server

--- src/broadcast_server.ts
+++ src/broadcast_server.ts
@@ -1,12 +1,12 @@
 import WebSocket, { Server } from 'ws';
+import express, { Request, Response, NextFunction } from 'express'
+import { createServer } from 'http'
 
-const map = new Map<string, any>();
+let wss: Server
 
-const wss = new Server({ port: 8081 });
-
-const broadcast = (data: any) => {
+const broadcast = (data: any, ws: any) => {
     wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
             try {
                 client.send(data)
             } catch(err) {
@@ -16,20 +16,72 @@ const broadcast = (data: any) => {
     })
 }
 
-wss.on('connection', (ws, req) => { // req.headers
-    ws.on('message', data => {
-        console.log(`Join: '${data}'`);
+const getHandler = (req: Request, res: Response) => {
+    // send WS notification
+    //curWs.send('notif from getHandler...')
 
-        (ws as any).id = data; // typeof Buffer
+    // broadcast
+    //broadcast('User created !')
 
-        //ws.send(`Processing: '${data}'`);
-        broadcast(`Join: ${data}`)
+    res.status(200).send('Users list coming soon...')
+}
+
+const bootstrap = () => {
+    const app = express();
+    const mapConn = new Map();
+
+    //
+    // Serve static files from the 'public' folder.
+    //
+    app.use(express.static('public'));
+    app.get('*', (req: Request, res: Response) => {
+        res.sendFile('index_broadcast.html', {root: 'public'});
     });
 
-    ws.on('close', (code: number, reason: string) => {
-        const leftUserId: Buffer = (ws as any).id
-        broadcast(`Left: ${leftUserId}`)
-    })
-});
+    //
+    // routes
+    //
+    app.get('/users', getHandler);
 
-console.log(`Listen for incoming requests on port 8081...`)
+    //
+    // Create an HTTP server
+    //
+    const server = createServer(app);
+
+    //
+    // Create a WebSocket server completely detached from the HTTP server.
+    //
+    wss = new Server({ noServer: true });
+
+    server.on('upgrade', (request, socket: any, head) => {
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            wss.emit('connection', ws, request);
+        });
+    });
+
+    wss.on('connection', (ws, req) => { // req.headers
+        ws.on('message', data => {
+            console.log(`Join: '${data}'`);
+    
+            (ws as any).id = data; // typeof Buffer
+    
+            //ws.send(`Processing: '${data}'`);
+            broadcast(`Join: ${data}`, ws)
+        });
+    
+        ws.on('close', (code: number, reason: string) => {
+            const leftUserId: Buffer = (ws as any).id
+            console.log(`Left: ${leftUserId}`)
+            broadcast(`Left: ${leftUserId}`, ws)
+        })
+    });
+
+    //
+    // start server
+    //
+    server.listen(4500, () => {
+        console.log(`HTTP server listen at port 4500...`)
+    })
+}
+
+bootstrap();
